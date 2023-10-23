5609fd9ad498c3ff1fdd77ca93a4a74c8194490e
amund-fremming <amund.fremming@gmail.com>
2023-10-23 10:38:47 +0200

    Fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <div className="fixed rounded-full bg-[#E4E2E9] h-20 w-20 bottom-[25px] right-[41%] z-10 flex justify-center items-center text-4xl font-extrabold text-[#39236C]">
-        <FaPlus />
+      <div className="fixed bottom-[25px] w-full h-20 z-0 flex justify-center items-center">
+        <div className="z-10 rounded-full bg-[#E4E2E9] h-20 w-20 flex justify-center items-center text-4xl font-extrabold text-[#39236C]"></div>
       </div>
 
       <div className="shadow-2xl fixed bottom-0 left-0 w-full bg-[#E4E2E9] h-20 flex justify-between items-center pr-4 pl-4 text-3xl text-[#806FA3]">
@@ -18,7 +18,9 @@ export default function Navbar() {
         <Link to="/inbox">
           <FaInbox />
         </Link>
-        <div className="w-10" />
+        <div className="mb-14 text-4xl text-[#39236C]">
+          <FaPlus />
+        </div>
         <Link to="/cashpoints">
           <FaMoneyBillAlt />
         </Link>
